43a175262d1b2f80c5950c1991c4922d7bcf656e
Emirhan Kaplan <emskaplann@gmail.com>
2020-10-22 19:51:57 -0400

    savepoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+    def is_date?(date_str)
+        begin
+            splitted_str = date_str.split(/\W+/)
+            for i in 0..splitted_str.length - 1
+              if(splitted_str[i].length < 2)
+                splitted_str[i] = "0#{splitted_str[i]}"
+              end
+            end
+        rescue
+            return false
+        end
+        temp = splitted_str.join()
+        ['%m%d%Y','%m%d%y','%M%D%Y','%M%D%y'].each do |f|
+        begin
+            return true if Date.strptime(temp, f)
+            rescue
+                #do nothing
+            end
+        end
+        return false
+    end
 end
--- app/controllers/text_data_files_controller.rb
+++ app/controllers/text_data_files_controller.rb
@@ -39,22 +39,33 @@ class TextDataFilesController < ApplicationController
             # End of processing ID Column
 
             # Processing Timestamp Column
-            begin
-                Date.parse(row[timestamp_header])
-            rescue ArgumentError
-                non_convertible_timestamp_id_list.push(row[id_header])
-            rescue TypeError
-                non_convertible_timestamp_id_list.push(row[id_header])
+            if(!is_date?(row[timestamp_header])) 
+                non_convertible_timestamp_id_list.push(row[id_header]) 
             end
             # End of processing Timestamp Column
         end
 
         if(duplicate_id_list.length > 0 || non_convertible_timestamp_id_list.length > 0) 
-            # return error to the client and don't upload it.
-            render json: {error: true}
+            # return error to the client and don't import it into the system.
+            render json: {error: true, success: false, duplicate_id_list_length: duplicate_id_list.length, non_convertible_timestamp_id_list_length: non_convertible_timestamp_id_list.length, duplicate_id_list: duplicate_id_list, non_convertible_timestamp_id_list: non_convertible_timestamp_id_list}, :status => :success
         else
             # upload the file to the db and return success code to the client
-            render json: {success: true}
+
+            # write the file
+            file_location = "./app/controllers/csv_files/#{params[:file].original_filename}"
+            File.write(file_location, csv_table)
+
+            # upload to the database
+            cloudinary_response = Cloudinary::Uploader.upload(file_location, :resource_type => :raw)
+            file_url = cloudinary_response["url"]
+            # example response => {"asset_id"=>"12e15428161cf93a8d1688285b2dea63", "public_id"=>"gfxtbfeypd0hdvgrofaw.csv", "version"=>1603398664, "version_id"=>"455c519ac2c0af576b99a6da6d699e0c", "signature"=>"69e12c780e1a2b0c34923e9cce956892994c58fa", "resource_type"=>"raw", "created_at"=>"2020-10-22T20:31:04Z", "tags"=>[], "bytes"=>58647, "type"=>"upload", "etag"=>"9596578e31387b53f4e25dcf53b7bb72", "placeholder"=>false, "url"=>"http://res.cloudinary.com/dsvgmn8lx/raw/upload/v1603398664/gfxtbfeypd0hdvgrofaw.csv", "secure_url"=>"https://res.cloudinary.com/dsvgmn8lx/raw/upload/v1603398664/gfxtbfeypd0hdvgrofaw.csv", "original_filename"=>"company_data"}
+
+            # delete it on local
+            File.delete(file_location)
+
+            # create the object and return necessary values to the front end
+            new_file = TextDataFiles.create(:id => id_header, :name => name_header, :timestamp => timestamp_header, :link => file_url)
+            render json: {success: true, error: false, url: new_file.link, id: new_file.id, name: new_file.name, timestamp: new_file.timestamp}, :status => :success
         end
     end
 end
