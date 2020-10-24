5a0273cc4c4274b55ce46b1fe48f05e8a164deae
Emirhan Kaplan <emskaplann@gmail.com>
2020-10-24 13:05:57 -0400

    nil value bug fixed

--- app/models/text_data_file.rb
+++ app/models/text_data_file.rb
@@ -1,7 +1,3 @@
 class TextDataFile < ApplicationRecord
     include ActiveModel::Serialization
-    attr_accessor :id, :name, :timestamp, :link
-    def attributes
-        {'id' => nil, 'name' => nil, 'timestamp' => nil, 'link' => nil}
-    end
 end
